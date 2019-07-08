d2bc3144d327a231a99bee4f76bbd81046136f24
middleprince <hellangel.ah@gmail.com>
2019-07-08 17:09:15 +0800

    feat: Add traffic simulation project L1

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/src/Graphics.cpp" "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/src/Intersection.cpp" "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/src/Street.cpp" "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/src/TrafficObject.cpp" "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/src/TrafficSimulator-L1.cpp" "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-L1.cpp.o"
+  "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/src/Vehicle_Student.cpp" "/Users/middle_prince/myMIND/CPP_LREAN/CPP-Nano-Degree/Concurrency/course-1/project/L1_Project/build/CMakeFiles/traffic_simulation.dir/src/Vehicle_Student.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
